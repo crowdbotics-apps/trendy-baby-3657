46d6aab728c101587d2641a299bba214f417fa1b
Crowdbotics <team@crowdbotics.com>
2019-05-21 21:22:53 +0000

    initial commit

---
+++ android/app/src/main/java/com/trendy_baby_3657/MainActivity.java
@@ -0,0 +1,15 @@
+package com.trendy_baby_3657;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "trendy_baby_3657";
+    }
+}
